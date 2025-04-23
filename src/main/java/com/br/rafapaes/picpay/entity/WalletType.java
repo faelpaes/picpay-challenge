78fb58039de9fac87796c191f66fbdfde841608b
Rafael Paes <rafaelpaes.al@gmail.com>
2025-04-22 22:30:37 -0300

    chore: mapping of Wallet and WalletType entities

---
+++ src/main/java/com/br/rafapaes/picpay/entity/WalletType.java
@@ -0,0 +1,37 @@
+package com.br.rafapaes.picpay.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "tb_wallet_type")
+public class WalletType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public WalletType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
